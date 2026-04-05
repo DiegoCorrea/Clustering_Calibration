1d021b743b3e772556298d339d1214fe04f3568f
DiegoCorrea <diego.correa@aau.at>
2026-04-05 16:51:00 +0200

    step 1 on server

--- shell/step1.sh
+++ shell/step1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ./pierre_in_frame &&
-#python3 step1_preprocessing.py from_file=YES file_name=ml-1m_cross &&
-#python3 step1_preprocessing.py from_file=YES file_name=ml-1m_class_distribution_cross &&
-#python3 step1_preprocessing.py from_file=YES file_name=ml-1m_distribution_cross &&
-#python3 step1_preprocessing.py from_file=YES file_name=ml-1m_ohe_cross &&
+python3 step1_preprocessing.py from_file=YES file_name=ml-1m_cross &&
+python3 step1_preprocessing.py from_file=YES file_name=ml-1m_class_distribution_cross &&
+python3 step1_preprocessing.py from_file=YES file_name=ml-1m_distribution_cross &&
+python3 step1_preprocessing.py from_file=YES file_name=ml-1m_ohe_cross &&
 python3 step1_preprocessing.py from_file=YES file_name=yahoo_movies_cross &&
 python3 step1_preprocessing.py from_file=YES file_name=yahoo_movies_class_distribution_cross &&
 python3 step1_preprocessing.py from_file=YES file_name=yahoo_movies_distribution_cross &&
