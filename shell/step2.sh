73a1a5560b30dcfab9e879eed006b507a6dc8fad
DiegoCorrea <diego.correa@aau.at>
2026-04-06 15:39:30 +0200

    adding SVD to step 2

--- shell/step2.sh
+++ shell/step2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ./pierre_in_frame &&
 python3 step2_searches.py from_file=YES file_name=ml-1m_cross &&
-python3 step2_searches.py from_file=YES file_name=ml-1m_cross_conformity &&
 python3 step2_searches.py from_file=YES file_name=yahoo_movies_cross &&
-python3 step2_searches.py from_file=YES file_name=yahoo_movies_cross_conformity
+python3 step2_searches.py from_file=YES file_name=yahoo_movies_cross_conformity &&
+python3 step2_searches.py from_file=YES file_name=ml-1m_cross_conformity
