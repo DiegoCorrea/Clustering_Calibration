fd6942db566f449d5e3c222b1eb1c5575c24fc0d
DiegoCorrea <diego.correa@aau.at>
2026-04-09 22:59:47 +0200

    update Step 6

--- shell/step6.sh
+++ shell/step6.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd ./pierre_in_frame &&
 python3 step6_protocol.py from_file=YES file_name=ml-1m_cross &&
-python3 step6_protocol.py from_file=YES file_name=yahoo_movies_cross
+python3 step6_protocol.py from_file=YES file_name=ml-1m_cross_cross_conformity &&
+python3 step6_protocol.py from_file=YES file_name=yahoo_movies_cross &&
+python3 step6_protocol.py from_file=YES file_name=yahoo_movies_cross_conformity &&
+python3 step6_protocol.py from_file=YES file_name=twitter_movies_cross &&
+python3 step6_protocol.py from_file=YES file_name=twitter_movies_cross_conformity
