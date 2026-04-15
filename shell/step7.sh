8746959d8cc9b9b854de44fb6fae7ef0077d76f9
DiegoCorrea <diego.correa@aau.at>
2026-04-15 17:21:37 +0200

    update Step 7

--- shell/step7.sh
+++ shell/step7.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd ./pierre_in_frame &&
-#python3 step7_charts_tables.py from_file=YES file_name=ml-1m &&
-python3 step7_charts_tables.py from_file=YES file_name=yahoo_movies
+python3 step7_charts_tables.py from_file=YES file_name=ml-1m_cross &&
+python3 step7_charts_tables.py from_file=YES file_name=ml-1m_cross_cross_conformity &&
+python3 step7_charts_tables.py from_file=YES file_name=yahoo_movies_cross &&
+python3 step7_charts_tables.py from_file=YES file_name=yahoo_movies_cross_conformity &&
+python3 step7_charts_tables.py from_file=YES file_name=twitter_movies_cross &&
+python3 step7_charts_tables.py from_file=YES file_name=twitter_movies_cross_conformity
